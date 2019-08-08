c1f2aa44616af6d6cd53eec36ceeb4f5b4783742
Nick <33926351+Sornick88@users.noreply.github.com>
2019-08-08 07:12:35 +0700

    foo.c
    refactor on review.
    change naming: delete foo_ tag from methods name.
    change data asserts in methods:
    init, deinit, read_data, write_data, read_str, write str
    delete str obj  nullptr assert in read_str method 
    
    add const modyfier to source data for io methods

--- C/foo.c
+++ C/foo.c
@@ -8,22 +8,36 @@
 /*Private types*/
 typedef struct _tag_foo_data_t
 {
+  /*use method list pointer as obj type veryfication*/
+  foo_m_list_t  * p_m_list;
+  /*to use custom types they must be declared in .h file*/
+  foo_str_type_t  str; 
+  /*standart type*/
   uint32_t data;
 } foo_data_t;
 /*Private functions prototypes*/
-static void *   foo_init(void * p_context);
-static void     foo_deinit(void * p_context);
-static uint32_t foo_read(void * p_context, 
-                char * p_data, uint32_t data_len);
-static uint32_t foo_write(void * p_context, 
-                char * p_data, uint32_t data_len);
+static void * init(void * p_context);
+static void   deinit(void * p_context);
+
+static uint32_t read_data(void const * p_context, 
+                uint32_t * p_data);
+static uint32_t read_str(void const * p_context, 
+                foo_str_type_t * p_data);
+
+static uint32_t write_data(void * p_context, 
+                uint32_t const * p_data);
+static uint32_t write_str(void * p_context, 
+                foo_str_type_t const * p_data);
+
 /*Exported constants*/
 const foo_m_list_t foo_m_list = 
 {
-  .init     = foo_init,
-  .deinit   = foo_deinit,
-  .read     = foo_read,
-  .write    = foo_write,
+  .init         = init,
+  .deinit       = deinit,
+  .read_data    = read_data,
+  .read_str     = read_str,
+  .write_data   = write_data,
+  .write_str    = write_str,
 };
 /*External variables*/
 
@@ -36,43 +50,92 @@ const foo_m_list_t foo_m_list =
 /*Private constants*/
 
 /*Private functions*/
-static void *   foo_init(void * p_context)
+static void * init(void * p_context)
 {
-  return malloc(sizeof(foo_data_t));
+  foo_data_t * p_foo = NULL;
+  p_foo = malloc(sizeof(foo_data_t));
+  if (!p_foo)
+    return NULL;
+
+  p_foo->p_m_list = &foo_m_list;
+  p_foo->data = 0;
+  p_foo->str.str = NULL;
+  p_foo->str.str_len = 0;
+
+  return (void *)p_foo;
 }
-static void     foo_deinit(void * p_context)
+
+static uint32_t deinit(void * p_context)
 {
+  /*non foo class pointer*/
+  if (((foo_data_t *)p_context)->p_m_list != &foo_m_list)
+    return -1;
+
   free(p_context);
+
+  return 1;
 }
-static uint32_t foo_read(void * p_context, 
-                char * p_data, uint32_t data_len)
+
+static uint32_t read_data(void const * p_context, 
+                uint32_t * p_data)
 {
-  uint32_t ret_val = 0;
-  if (p_context && p_data && data_len == sizeof(foo_data_t))
-  {
-    ((foo_data_t*)p_data)->data = ((foo_data_t*)p_context)->data;
-    ret_val = 1;
-  }
-  else
-  {
-    ret_val = -1;
-  }
-  return ret_val;
+  if (!p_context && !p_data)
+    return -1;
+  
+  /*non foo class pointer*/
+  if (((foo_data_t *)p_context)->p_m_list != &foo_m_list)
+    return -1;
+  
+  *p_data = ((foo_data_t *)p_context)->data;
+
+  return 1;
 }
-static uint32_t foo_write(void * p_context, 
-                char * p_data, uint32_t data_len)
+
+static uint32_t read_str(void const * p_context, 
+                foo_str_type_t * p_str)
 {
-  uint32_t ret_val = 0;
-  if (p_context && p_data && data_len == sizeof(uint32_t))
-  {
-    ((foo_data_t*)p_context)->data = ((foo_data_t*)p_data)->data;
-    ret_val = 1;
-  }
-  else
-  {
-    ret_val = -1;
-  }
-  return ret_val;
+  if (!p_context && !p_data)
+    return -1;
+  
+  /*non foo class pointer*/
+  if (((foo_data_t *)p_context)->p_m_list != &foo_m_list)
+    return -1;
+
+  p_str->data = ((foo_data_t*)p_context)->str.data;
+  p_str->str_len = ((foo_data_t*)p_context)->str.str_len;
+
+  return 1;
+}
+
+static uint32_t write_data(void * p_context, 
+                uint32_t const * p_data)
+{
+  if (!p_context && !p_data)
+    return -1;
+  
+  /*non foo class pointer*/
+  if (((foo_data_t *)p_context)->p_m_list != &foo_m_list)
+    return -1;
+
+  ((foo_data_t*)p_context)->data = *p_data;
+
+  return 1;
+}
+
+static uint32_t write_str(void * p_context, 
+                foo_str_type_t * p_str)
+{
+  if (!p_context && !p_data)
+    return -1;
+  
+  /*non foo class pointer*/
+  if (((foo_data_t *)p_context)->p_m_list != &foo_m_list)
+    return -1;
+
+  ((foo_data_t*)p_context)->str.data = p_str->data;
+  ((foo_data_t*)p_context)->str.str_len = p_str->str_len;
+
+  return 1;
 }
 /*Public functions*/
 
--- C/foo.h
+++ C/foo.h
@@ -13,19 +13,33 @@
 
 // clang-format on
 /*Exported types*/
+typedef _tag_foo_str_type_t
+{
+	char * data;
+	uint32_t 	str_len
+}	foo_str_type_t;
 // clang-format off
-typedef void * (foo_init_t)(void * p_context);
-typedef void (foo_deinit_t)(void * p_context);
-typedef uint32_t (foo_io_t)(void * p_context, 
-                char * p_data, uint32_t data_len);
+typedef void * (*foo_init_t)(void * p_context);
+typedef void (*foo_deinit_t)(void * p_context);
 
+typedef uint32_t (*foo_read_data_t)(void const * p_context, 
+                uint32_t * p_data);
+typedef uint32_t (*foo_write_data_t)(void * p_context, 
+                uint32_t const * p_data);
+								
+typedef uint32_t (*foo_read_str_t)(void const * p_context, 
+                foo_str_type_t * p_str);
+typedef uint32_t (*foo_write_str_t)(void * p_context, 
+                foo_str_type_t const * p_str);
 // clang-format on
 typedef struct _tag_foo_m_list_t
 {
-	foo_init_t 	  init;
-	foo_deinit_t 	deinit;
-	foo_io_t	    read;
-	foo_io_t	    write;
+	foo_init_t 	  		init;
+	foo_deinit_t 			deinit;
+	foo_read_data_t	  read_data;
+	foo_read_str_t		read_str;
+	foo_write_data_t	write_data;
+	foo_write_str_t		write_str;
 }	foo_m_list_t;
 
 extern const foo_m_list_t foo_m_list;
